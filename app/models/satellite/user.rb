f0bfa8680b637638a3d52c9df96a3900faf428c6
niuage <niuage@gmail.com>
2016-01-28 11:29:18 -0500

    the name prop is now provided by omniauth-devpost directly

--- app/models/satellite/user.rb
+++ app/models/satellite/user.rb
@@ -8,18 +8,15 @@ module Satellite
       def create_with_omniauth(auth)
         auth.extend Hashie::Extensions::DeepFetch
         create! do |user|
-          user.provider = auth['provider']
-          user.uid      = auth['uid']
-          user.name  = [
-            auth.deep_fetch('info', 'first_name') { '' } ,
-            auth.deep_fetch('info', 'last_name') { '' }
-          ].join(' ')
-          user.email = auth.deep_fetch('info', 'email') { "" }
+          user.provider = auth["provider"]
+          user.uid      = auth["uid"]
+          user.name  = auth.deep_fetch("info", "name") { "" }
+          user.email = auth.deep_fetch("info", "email") { "" }
         end
       end
 
       def find_with_omniauth(auth)
-        find_by(provider: auth['provider'], uid: auth['uid'].to_s)
+        find_by(provider: auth["provider"], uid: auth["uid"].to_s)
       end
 
       def find_or_create_with_omniauth(auth)
