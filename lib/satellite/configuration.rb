9e826b97ab9b5c0c1f13f02a00df41e71bc6ec6b
niuage <niuage@gmail.com>
2016-01-29 15:13:01 -0500

    another attempt, changing the path to ca

--- lib/satellite/configuration.rb
+++ lib/satellite/configuration.rb
@@ -72,7 +72,7 @@ module Satellite
         opts = config.omniauth_args.extract_options!
         provider config.provider, *config.omniauth_args, opts.merge(
           path_prefix: config.path_prefix,
-          client_options: {:ssl => {:ca_path => "/etc/ssl/certs"}}
+          client_options: {:ssl => {:ca_path => '/usr/lib/ssl/certs/ca-certificates.crt'}}
         )
       end
     end
